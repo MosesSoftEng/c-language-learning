2c8eeb2602d507b19ac6e5c2279da2d07879679e
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-12 09:08:12 +0300

    Define struct structure

--- data-structures/linkedlist.c
+++ data-structures/linkedlist.c
@@ -2,5 +2,10 @@
 /* gcc -Wall -pedantic -Werror -Wextra -std=gnu89 linkedlist.c -o linkedlist && ./linkedlist */
 
 /* Define struct structure */
+typedef struct linkedlist_s
+{
+	char *data;
+	struct ls_t *next;
+} ls_t;
 
 /* Define main function */
