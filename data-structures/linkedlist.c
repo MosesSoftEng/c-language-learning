9ff6ddba16425e0b1d8d631e9fb2ad3dd1bd004a
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-12 09:07:51 +0300

    Terminal compilation command

---
+++ data-structures/linkedlist.c
@@ -0,0 +1,6 @@
+/* Terminal compilation command */
+/* gcc -Wall -pedantic -Werror -Wextra -std=gnu89 linkedlist.c -o linkedlist && ./linkedlist */
+
+/* Define struct structure */
+
+/* Define main function */
