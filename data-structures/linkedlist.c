ca61a491e419e7914495c82d61b94534425daa6e
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-12 09:11:16 +0300

    Create head

--- data-structures/linkedlist.c
+++ data-structures/linkedlist.c
@@ -11,5 +11,14 @@ typedef struct linkedlist_s
 /* Define main function */
 int main(void)
 {
+	/* Create head */
+	ls_t *head;
+	
+	/* Insert element*/
+	/* Insert element at the end - Push */
+	/* Insert element at the head */
+	/* Insert element at the middle */
+
+	/* List all elements*/
 	return (0);
 }
