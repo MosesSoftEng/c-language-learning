9d4729301bb30f77efa66d9f2bad83176842a409
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-12 11:57:59 +0300

    Insert element at the end of linked list

--- data-structures/linkedlist.c
+++ data-structures/linkedlist.c
@@ -1,24 +1,34 @@
 /* Terminal compilation command */
 /* gcc -Wall -pedantic -Werror -Wextra -std=gnu89 linkedlist.c -o linkedlist && ./linkedlist */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /* Define struct structure */
 typedef struct linkedlist_s
 {
 	char *data;
-	struct ls_t *next;
+	struct linkedlist_s *next;
 } ls_t;
 
-void ls_add(ls_t *head, char *data);
+int ls_push(ls_t **head, char *data);
 
 /* Define main function */
 int main(void)
 {
-	/* Create head */
-	ls_t *head;
+	/* Create head pointer*/
+	ls_t *head = NULL;
 
 	/* Insert element*/
 	/* Insert element at the end - Push */
-	ls_add(head, "data one");
+	ls_push(&head, "data one");
+	printf("%s\n", head->data);
+
+	/* Go to next node */
+	head = head->next;
+
+	ls_push(&head, "data two");
+	printf("%s\n", head->data);
 
 	/* Insert element at the head */
 	/* Insert element at the middle */
@@ -27,7 +37,29 @@ int main(void)
 	return (0);
 }
 
-void ls_add(ls_t *head, char *data)
+int ls_push(__attribute__((unused)) ls_t **head, __attribute__((unused)) char *data)
 {
+	/* Create new node pointer*/
+	ls_t *new_node, *end_node;
 
+	new_node = malloc(sizeof(ls_t));
+
+	new_node->data = data;
+
+	/* Attach node to end */
+	/* First add */
+	if (*head == NULL)
+		*head = new_node;
+	/* Subsequent add */
+	else
+	{
+		end_node = *head;
+
+		while(end_node->next != NULL) /* Go to last node */
+			end_node = end_node->next;
+
+		end_node->next = new_node;
+	}
+
+	return (0);
 }
